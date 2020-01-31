f32f0c1b8a3cdb6765296ff1e9812e971245aea1
Gilberto Bittencourt <gilbertobitt@outlook.com>
2020-01-31 16:34:11 -0300

    Updating init project

---
+++ Assets/Scripts/Behavior/MoveBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Behavior
+{
+    public class MoveBehavior : MonoBehaviour
+    {
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
